b862026db32c242c5f4a4facc33f308505c31b27
Joanna Baile <joanna@studentsofferingsupport.org>
2019-03-13 13:43:51 -0400

    added cats

---
+++ data.js
@@ -0,0 +1,20 @@
+const data = {
+    name: "Joanna",
+    role: "admin",
+
+    user1: {
+        name: "user",
+        role: "developer",
+        bio: "some information about the person goes here",
+        bioPic: "cat1.jpg"
+    },
+
+    user2: {
+        name: "user2",
+        role: "designer",
+        bio: "some information about the person goes here",
+        bioPic: "cat2.jpg"
+    }
+};
+
+module.exports = data;
